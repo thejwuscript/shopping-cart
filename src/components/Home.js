17d2501e150dbe17fdde3b10b388822222f14e91
James Wu <ichangjwu@outlook.com>
2022-08-11 14:14:33 +0900

    Integreate Home with App

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../images/hero.jpg';
 
 export default function Home() {
   return (
-    <div>
-      <img src={Hero} alt="hero" />
+    <div id="home">
+      <Link to="/shop"><img src={Hero} alt="hero" id="hero" /></Link>
       <h1>Welcome!</h1>
     </div>
   );
--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,17 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import Home from './Home';
+import { BrowserRouter } from 'react-router-dom';
+
+const MockHome = () => (
+  <BrowserRouter>
+    <Home />
+  </BrowserRouter>
+)
 
 describe('Home', () => {
   it('renders the hero image', () => {
-    render(<Home />);
+    render(<MockHome />);
     const image = screen.getByAltText('hero');
-    
     expect(image.src).toContain('hero.jpg');
   })
 
   it('displays a welcome message', () => {
-    render(<Home />);
+    render(<MockHome />);
     const message = screen.getByRole('heading', { name: 'Welcome!'});
     expect(message).toBeInTheDocument();
+  })
+
+  it('renders a link to the Shop page', () => {
+    render(<MockHome />);
+    const link = screen.getByRole('link')
+    expect(link).toHaveAttribute('href', '/shop');
   })
 });
