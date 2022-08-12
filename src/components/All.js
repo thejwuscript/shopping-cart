22904258e100bdbb6115da5c7c33218de784ee3d
James Wu <ichangjwu@outlook.com>
2022-08-12 22:34:34 +0900

    Style grid

--- src/components/All.js
+++ src/components/All.js
@@ -7,9 +7,16 @@ export default function All({ games }) {
         {games.map((game) => {
           return (
             <li key={game.id} className="game-item">
-              <img src={game.images.small} alt="" width={150} height={150} />
+              <img src={game.images.small} alt="" width={140} height={140} />
               <h3>{game.name}</h3>
               <span>${game.price_ca}</span>
+              <form>
+                <button type="submit">Add to Cart</button>
+                <div>
+                  <label htmlFor="quantity">Qty: </label>
+                  <input type="number" id="quantity" name="quantity" min={0} max={5} />
+                </div>
+              </form>
             </li>
           );
         })}
