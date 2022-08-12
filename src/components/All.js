ac97cc4b88f5bf15bb0bfb3479b28fbbfdc7665d
James Wu <ichangjwu@outlook.com>
2022-08-12 17:08:17 +0900

    Fix snapshot testing with act

--- src/components/All.js
+++ src/components/All.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function All() {
-  const [games, setGames] = useState([])
+  const [games, setGames] = useState([]);
 
   useEffect(() => {
-    fetch('https://api.boardgameatlas.com/api/search?limit=2&client_id=R7JYD2LOOW')
-      .then(response => response.json())
-      .then(data => {
-        setGames(data.games);
-      })
-  }, [])
+    async function fetchData() {
+      const response = await fetch(
+        "https://api.boardgameatlas.com/api/search?limit=2&client_id=R7JYD2LOOW"
+      );
+      const data = await response.json();
+      setGames(data.games);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       <ul>
-        {games.map(game => {
+        {games.map((game) => {
           return <li key={game.id}>{game.name}</li>;
         })}
       </ul>
     </div>
   );
-};
+}
--- src/components/All.test.js
+++ src/components/All.test.js
@@ -1,5 +1,7 @@
-import { screen, render } from '@testing-library/react';
-import All from './All';
+import { screen, render } from "@testing-library/react";
+import renderer from 'react-test-renderer';
+import { act } from "react-dom/test-utils";
+import All from "./All";
 
 const MOCK_RESPONSE = {
   games: [
@@ -10,23 +12,33 @@ const MOCK_RESPONSE = {
     {
       id: "two",
       name: "Game Two",
-    }
-  ]
+    },
+  ],
 };
 
-global.fetch = () => Promise.resolve({
-  json: () => Promise.resolve(MOCK_RESPONSE)
-})
-
-describe('All', () => {
-  it('renders without crashing', () => {
-    render(<All />);
+global.fetch = () =>
+  Promise.resolve({
+    json: () => Promise.resolve(MOCK_RESPONSE),
   });
 
-  it('renders a list of games from all categories', async () => {
-    render(<All />);
-    // expect(await screen.findByText('Game One')).toBeInTheDocument();
-    // expect(await screen.findByText('Game Two')).toBeInTheDocument();
-  })
-})
+describe("All", () => {
+  it("renders properly", async () => {
+    let component;
+    await act(async () => {
+      component = renderer.create(<All />);
+    });
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 
+  it("renders all games", async () => {
+    render(<All />);
+    expect(await screen.findByText('Game One')).toBeInTheDocument();
+    expect(await screen.findByText('Game Two')).toBeInTheDocument();
+  });
+
+  // it('renders a list of games from all categories', async () => {
+  //   render(<All />);
+  //   expect(await screen.findByText('Game One')).toBeInTheDocument();
+  //   // expect(await screen.findByText('Game Two')).toBeInTheDocument();
+  // })
+});
