a8563da3b5ce0c84f10eecf34730efb7c84f8120
James Wu <ichangjwu@outlook.com>
2022-08-12 14:52:13 +0900

    Add tests to All

--- src/components/All.js
+++ src/components/All.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function All() {
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.boardgameatlas.com/api/search?limit=2&client_id=R7JYD2LOOW')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data.games);
+      })
+  }, [])
+
   return (
-    <div>all games</div>
-  )
+    <div>
+      <ul>
+        {games.map(game => {
+          return <li key={game.id}>{game.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
--- src/components/All.test.js
+++ src/components/All.test.js
@@ -1,8 +1,32 @@
 import { screen, render } from '@testing-library/react';
 import All from './All';
 
+const MOCK_RESPONSE = {
+  games: [
+    {
+      id: "one",
+      name: "Game One",
+    },
+    {
+      id: "two",
+      name: "Game Two",
+    }
+  ]
+};
+
+global.fetch = () => Promise.resolve({
+  json: () => Promise.resolve(MOCK_RESPONSE)
+})
+
 describe('All', () => {
   it('renders without crashing', () => {
     render(<All />);
+  });
+
+  it('renders a list of games from all categories', async () => {
+    render(<All />);
+    // expect(await screen.findByText('Game One')).toBeInTheDocument();
+    // expect(await screen.findByText('Game Two')).toBeInTheDocument();
   })
 })
+
