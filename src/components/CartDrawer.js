827dbe7be2ea00b812e8092286f4243484f4396e
James Wu <ichangjwu@outlook.com>
2022-08-13 19:38:49 +0000

    Add transition to cart drawer

--- src/components/CartDrawer.js
+++ src/components/CartDrawer.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function CartDrawer() {
+  const [visible, setVisible] = useState(false);
+  
   return (
     <div className="drawer-container">Your cart is empty.</div>
   );
--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -3,8 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function CartIcon({ count }) {
+  const handleClick = () => {
+    const drawer = document.querySelector('.drawer-container');
+    drawer.classList.add('active')
+  }
+
   return (
-    <div aria-label='shopping-cart' className="cart-wrapper">
+    <div aria-label='shopping-cart' className="cart-wrapper" onClick={handleClick}>
       <FontAwesomeIcon icon={faCartShopping} />
       <span className='badge'>{count}</span>
     </div>
